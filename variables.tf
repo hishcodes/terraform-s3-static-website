3d4a2511aee9dca2ec97670aa5369e02a488ff3d
hishcodes <hishamkumbra@gmail.com>
2024-11-28 16:06:47 +0530

    comment

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "bucket_name" {
   description = "Bucket name"
-  default = "hishcodes-demo-site"
+  default = "hishcodes-demo-site" #Enter s3 bucket name here and must be unique.
 }
 
 variable "bucket_tag_name" {
